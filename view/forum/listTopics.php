490671a19c2eef3ab829959037d395bf222629f1
JManciet <manciet@hotmail.fr>
2022-10-27 00:09:42 +0200

    Amélioration front liste topics

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -5,34 +5,42 @@ $topics = $result["data"]['topics'];
 
 ?>
 
-<h1>liste topics</h1>
+<div id="titreTopics">
+    <h1>liste topics</h1>
+</div>
 
 <?php
 foreach($topics as $topic ){
     ?>
     <p>
-    <div><a href="index.php?ctrl=forum&action=posts&id=<?=$topic->getId()?>"><?=$topic->getTitle()?></a> <?php echo ($topic->getClosed() ?'<i class="fa-solid fa-lock"></i>' :'<i class="fa-solid fa-lock-open"></i>') ?></div>
-    <div><?=$topic->getCreationdate()?></div>
-    <div>Nombre de reponse : <?php 
-    $topicManager = new TopicManager(); 
-    echo $topicManager->nbrPostByTopic($topic->getId())["nbrmessage"];;
-    ?>
-    </div>
-    <div><?php
+        <div onclick="location.href='index.php?ctrl=forum&action=posts&id=<?=$topic->getId()?>';" class="topicContainer">
+            <div class="topicTitle">
+                <div><a href="index.php?ctrl=forum&action=posts&id=<?=$topic->getId()?>"><?=$topic->getTitle()?></a></div>
+                <div><?php echo ($topic->getClosed() ?'<i class="fa-solid fa-lock"></i>' :'<i class="fa-solid fa-lock-open"></i>') ?></div>
+            </div>
+            <div><strong>par ... </strong> le <?=$topic->getCreationdate()?></div>
+            <div class="topicNbrPost">
+                <div>Nombre de posts : <?php 
+                $topicManager = new TopicManager(); 
+                echo $topicManager->nbrPostByTopic($topic->getId())["nbrmessage"];;
+                ?>
+                </div>
+                <div><?php
 
-    if($topic->getUser()->getId() == App\Session::getUser()->getId()){
+                if($topic->getUser()->getId() == App\Session::getUser()->getId()){
 
-        if($topic->getClosed()){
-            echo "<span class='badge text-bg-success'><a class='lock' href='index.php?ctrl=forum&action=toggClosed&id=".$topic->getId()."&from=forum'>Dévérouiller le topic</a></span>";
-        }else{
-            echo "<span class='badge text-bg-danger'><a class='lock' href='index.php?ctrl=forum&action=toggClosed&id=".$topic->getId()."&from=forum'>Vérouiller le topic</a></span>";
-        }
+                    if($topic->getClosed()){
+                        echo "<span class='badge text-bg-success'><a class='lock' href='index.php?ctrl=forum&action=toggClosed&id=".$topic->getId()."&from=forum'>Dévérouiller le topic</a></span>";
+                    }else{
+                        echo "<span class='badge text-bg-danger'><a class='lock' href='index.php?ctrl=forum&action=toggClosed&id=".$topic->getId()."&from=forum'>Vérouiller le topic</a></span>";
+                    }
 
-    }
-     
-     ?>
-
-    </div>
+                }
+                
+                ?>
+                </div>
+            </div>
+        </div>
     </p>
     <?php
 
